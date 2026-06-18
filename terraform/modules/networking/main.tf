40d21c748ba663ce752f72ba1eb68e9fcdec2805
Mpho Madisha <Mpho@MacBook-Air.local>
2026-06-18 09:07:13 +0200

    Add VPC subnets and routing

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -8,3 +8,83 @@ resource "aws_vpc" "this" {
     Environment = var.environment
   }
 }
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnets)
+
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnets[count.index]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "cvm-${var.environment}-public-subnet-${count.index + 1}"
+    Environment = var.environment
+    Type        = "public"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnets)
+
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name        = "cvm-${var.environment}-private-subnet-${count.index + 1}"
+    Environment = var.environment
+    Type        = "private"
+  }
+} 
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name        = "cvm-${var.environment}-igw"
+    Environment = var.environment
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name        = "cvm-${var.environment}-public-rt"
+    Environment = var.environment
+  }
+}
+
+resource "aws_route" "public_internet" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(aws_subnet.public)
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+} 
+
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name        = "cvm-${var.environment}-private-rt"
+    Environment = var.environment
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(aws_subnet.private)
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
