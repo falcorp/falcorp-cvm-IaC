71fe4b2762689b4d80aad6a6a443820f0bba877f
Mpho Madisha <Mpho@MacBook-Air.local>
2026-06-25 20:11:38 +0200

    Add Glue ETL and curated Athena query pipeline

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -4,7 +4,6 @@ module "networking" {
   environment = "dev"
   vpc_cidr    = "10.0.0.0/16"
 
-
   public_subnets = [
     "10.0.1.0/24",
     "10.0.2.0/24"
@@ -16,16 +15,45 @@ module "networking" {
   ]
 }
 
-module "s3" {
-  source = "../../modules/s3"
-
-  environment = "dev"
-  kms_key_arn = module.security.kms_key_arn
-}
-
 module "security" {
   source = "../../modules/security"
 
   environment = "dev"
 }
 
+module "s3" {
+  source = "../../modules/s3"
+
+  environment = "dev"
+  kms_key_arn = module.security.kms_key_arn
+}
+module "sns" {
+  source = "../../modules/sns"
+
+  environment = "dev"
+}
+module "eventbridge" {
+  source = "../../modules/eventbridge"
+
+  environment          = "dev"
+  bucket_name          = module.s3.bucket_name
+  lambda_arn           = module.lambda.lambda_arn
+  lambda_function_name = module.lambda.lambda_function_name
+}
+module "lambda" {
+  source = "../../modules/lambda"
+
+  environment   = "dev"
+  bucket_name   = module.s3.bucket_name
+  sns_topic_arn = module.sns.topic_arn
+  kms_key_arn   = module.security.kms_key_arn
+}
+
+module "glue" {
+  source = "../../modules/glue"
+
+  environment = "dev"
+  bucket_name = module.s3.bucket_name
+  bucket_arn  = module.s3.bucket_arn
+  kms_key_arn = module.security.kms_key_arn
+} 
