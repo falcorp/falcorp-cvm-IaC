998da5c7e7c81923482b8005df55184157186475
OseluFalcorp <tobi.enabor@falcorp.co.za>
2026-06-25 15:04:55 +0200

    Progress in development - creating factories to create fictious data for AWS cvm solution

---
+++ src/data_generator.py
@@ -0,0 +1,7 @@
+from faker import Faker
+class BaseDataGenerator:
+    def __init__(self, seed=None) -> None:
+        self.fake = Faker()
+        # setup seed
+        if seed is not None:
+            self.fake.seed_instance(seed)
---
+++ src/tests/test_customer_generator.py
@@ -0,0 +1,50 @@
+import pytest
+from pathlib import Path
+import os
+import sys
+from datetime import datetime
+
+from customer import CustomerFactory
+
+# Define temporary test path 
+TEST_OUTPUT_PATH = "../../data/test_customer_mock_data.csv"
+
+@pytest.fixture(autouse=True)
+def cleanup_test_files():
+    yield
+    if os.path.exists(TEST_OUTPUT_PATH):
+        os.remove(TEST_OUTPUT_PATH)
+@pytest.fixture(scope="class")
+def class_scoped_customer():
+    return CustomerFactory.generate_customer()
+class TestCustomerCreation:
+
+    def test_customer_attributes():
+        expected_attributes = [
+            "customer_id", "registration_date", "termination_date",
+            "date_of_birth", "gender", "signup_channel", 
+            "signup_promo_code", "ported_in_status",
+            "previous_carrier", "plan_id",
+            "boyd", "registration_device_tac",
+            "payment_method"
+        ]
+        
+        assert expected_attributes == list(class_scoped_customer.keys())
+
+    def test_customer_registration_date():
+
+        def convert_iso_to_date(target_iso):
+            try:
+                clean_iso = target_iso.replace("Z", "+00.00")
+                target_datetime = datetime.fromisoformat(clean_iso)
+                return target_datetime.date()
+            except ValueError as e:
+                print(f"Invalid ISO 8601 format: {e}")
+                return None
+        
+        start = datetime(2020,1,1)
+        end = datetime.now().date()
+        assert start <= convert_iso_to_date(class_scoped_customer.registration_date) <= end
+
+def test_generate_to_csv_creates_files():
+    pass
