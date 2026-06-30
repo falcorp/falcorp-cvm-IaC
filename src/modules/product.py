eb284ac6b0f7ff409f2fafb11b6d923d45f2e081
OseluFalcorp <tobi.enabor@falcorp.co.za>
2026-06-30 10:58:14 +0200

    created product data and included tests for outputs

---
+++ src/modules/product.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from modules.data_generator import BaseDataGenerator
+from modules.providers import ProductProvider
+
+# -----------------------------------------------------------
+# ProductFactory
+# -----------------------------------------------------------
+class ProductFactory(BaseDataGenerator):
+    def __init__(self, seed=None) -> None:
+        if seed is not None:
+            super().__init__(seed=seed)
+        else:
+            super().__init__()
+        # add providers
+        self.fake.add_provider(ProductProvider)
+    def generate_product(self):
+        return {
+            "product_id":self.fake.product_id(),
+            "product_name": self.fake.product_name(),
+            "product_type": self.fake.product_type(),
+            "billing_type": self.fake.billing_type(),
+            "product_price": self.fake.product_price(),
+            "data_gb_allowance": self.fake.data_gb_allowance(),
+            "voice_minutes_allowance": self.fake.voice_minutes_allowance(),
+            "sms_count_allowance": self.fake.sms_count_allowance()
+
+        }
---
+++ src/scripts/generate_products.py
@@ -0,0 +1,24 @@
+import random
+
+import pandas as pd
+from pathlib import Path
+import os
+import sys
+
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+from modules.product import ProductFactory
+
+if __name__ == "__main__":
+
+    SCRIPT_DIR = Path(__file__).resolve().parent
+    TARGET_DIR = SCRIPT_DIR.parent.parent / "data" / "products"
+    TARGET_DIR.mkdir(parents=True, exist_ok=True)
+
+    SEED = 42
+    number_of_products = 20
+    product_factory = ProductFactory(seed=SEED)
+    product_generator = (product_factory.generate_product() for _ in range(number_of_products))
+
+    df = pd.DataFrame(product_generator)
+    file_path = TARGET_DIR / "products.csv"
+    df.to_csv(file_path, index=False)
